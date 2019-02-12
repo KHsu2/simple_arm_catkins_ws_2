f4849e15ad4fea16d6ae0c688f35e0d3bcbea18e
Kevin <kevmohsu@gmail.com>
2019-02-12 20:30:00 +0000

    First Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkins_ws/src
+# Build directory: /home/workspace/catkins_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(simple_arm)
